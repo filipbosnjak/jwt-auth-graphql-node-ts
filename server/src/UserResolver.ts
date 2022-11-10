b8798e0f838b57f6607e30b80d8f7698d5e2123c
filipbosnjak <filipo.bosnjak@gmail.com>
2022-11-10 18:51:17 +0100

    register mutation init and user entity update

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,9 +1,17 @@
-import {Resolver, Query} from 'type-graphql'
+import {Resolver, Query, Mutation, Arg} from 'type-graphql'
 
 @Resolver()
 export class UserResolver {
     @Query(() => String)
     hello() {
         return "hi from resolvers"
+    }
+
+    @Mutation()
+    register(
+        @Arg('email', () => String) email: String,
+        @Arg('password', () => String) password: String
+    ) {
+
     }
 }
--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,7 +1,7 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm"
 
-@Entity()
-export class User {
+@Entity('users')
+export class User extends BaseEntity {
 
     @PrimaryGeneratedColumn()
     id: number
@@ -10,9 +10,9 @@ export class User {
     firstName: string
 
     @Column()
-    lastName: string
+    email: string
 
     @Column()
-    age: number
+    password: number
 
 }
