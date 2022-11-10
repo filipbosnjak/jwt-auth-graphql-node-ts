ab3e72477ada1bed02265cc97658ce8c3af64b5a
filipbosnjak <filipo.bosnjak@gmail.com>
2022-11-10 18:10:56 +0100

    setup express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,41 @@
-import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import express from 'express'
+import { Request, Response } from "express"
+import "reflect-metadata"
 
-AppDataSource.initialize().then(async () => {
+// Function that calls itself
+// (() => {})()
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+(async () => {
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
+    const app = express();
 
-    console.log("Here you can setup and run express / fastify / any other framework.")
+    app.get('/', (
+        _: Request, 
+        res: Response
+    ) => {
+        res.send("express")
+    })
 
-}).catch(error => console.log(error))
+    app.listen(4000, () => {
+        console.log("express server started");
+        
+    })
+})()
+
+// AppDataSource.initialize().then(async () => {
+
+//     console.log("Inserting a new user into the database...")
+//     const user = new User()
+//     user.firstName = "Timber"
+//     user.lastName = "Saw"
+//     user.age = 25
+//     await AppDataSource.manager.save(user)
+//     console.log("Saved a new user with id: " + user.id)
+
+//     console.log("Loading users from the database...")
+//     const users = await AppDataSource.manager.find(User)
+//     console.log("Loaded users: ", users)
+
+//     console.log("Here you can setup and run express / fastify / any other framework.")
+
+// }).catch(error => console.log(error))
